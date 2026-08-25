b8c1e283c3ea012c41f757e04e3c8d92fb9b932f
Ana Carolina Wade <128090198+NelithHub@users.noreply.github.com>
2026-08-25 17:17:50 -0300

    Create practica.py

---
+++ practica.py
@@ -0,0 +1,22 @@
+#numero = int("10")
+#print(numero * 2)
+
+#nombre = "Juan"
+
+#edad = 30
+
+#print("Hola,", nombre, "tenés", edad, "años.")
+
+#edad = input("Ingresá tu edad: ")
+
+#edad = int(edad)
+
+#print("Tu edad es:", edad)
+
+#x, y, z = 1, 2, 3
+
+#print(x + y + z)
+
+edad = "25"
+
+print(int(edad) + 5)
