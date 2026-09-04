a895a2930aa427d388be6619f5267f4227177ee5
Ana Carolina Wade <128090198+NelithHub@users.noreply.github.com>
2026-09-03 21:22:30 -0300

    Update practica.py

--- practica.py
+++ practica.py
@@ -20,3 +20,60 @@
 edad = "25"
 
 print(int(edad) + 5)
+
+#clase 4
+
+#edad= 125
+
+#if edad < 15:
+#    print("Es menor")
+#elif edad >= 13 and edad < 18:
+#    print ("Es adolecente")
+#elif edad >=18:
+#    print("Es adulto")
+#else:
+#    print("Edad no valida")
+
+if fruta == "manzana":
+    print("rojo o verde")
+elif fruta == "banana":
+    print("amarillo")
+elif fruta == "naranja":
+    print("anaranjado")
+else:
+    print("Desconocida")
+
+
+match fruta:
+    case "manzana":
+        print("rojo o verde")
+    case "banana":
+        print("amarillo")
+    case "naranja":
+        print("anaranjado")
+    case _:
+        print("Desconocida")
+
+
+#len()
+
+mensaje ="hola"
+print(len(mensaje)) #--> len() es una funcion que nos permite saber la cantidad de caracteres que tiene un string.
+
+nombre = input("ingresa tu nombre: ")
+
+if len(nombre) == 0:
+    print("eldato solicitado no cumple con lo esperado")
+elif len(nombre) > 2:
+    print("su nombre es:", nombre)
+
+
+    salir_prog =input("ingresa ´salir´ para salir del programa: ").strip().lower()
+
+    #upper()
+    if salir_prog == "salir":
+        print("programa terminado")
+else:
+    print("dsigo preguntado")
+
+print (mensaje[0])
