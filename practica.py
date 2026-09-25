0ec40f7fec01c0369388b6fadefdef679327a49c
Ana Carolina Wade <128090198+NelithHub@users.noreply.github.com>
2026-09-24 21:58:37 -0300

    Update practica.py

--- practica.py
+++ practica.py
@@ -323,70 +323,183 @@
 #     print("================================")
 
 
-libros = []
-opcion = ""
+#libros = []
+#opcion = ""
 
-while opcion != "4":
-    print("========== BIBLIOTECA ==========")
-    print("1. Ingresar libro")
-    print("2. Mostrar libros registrados")
-    print("3. Buscar libro por título")
-    print("4. Salir")
-    print("================================")
+#while opcion != "4":
+#    print("========== BIBLIOTECA ==========")
+#    print("1. Ingresar libro")
+#    print("2. Mostrar libros registrados")
+#    print("3. Buscar libro por título")
+#    print("4. Salir")
+#    print("================================")
 
-    opcion = input("Seleccione una opción: ").strip()
+#    opcion = input("Seleccione una opción: ").strip()
 
     #--------------Ingreso-----------------
-    if opcion == "1":
-        titulo = input("Titulo: ").strip()
-        while titulo == "":
-            print("El titulo no puede estar vacio")
-            titulo = input("Titulo: ").strip()
+#    if opcion == "1":
+#        titulo = input("Titulo: ").strip()
+#        while titulo == "":
+#            print("El titulo no puede estar vacio")
+#            titulo = input("Titulo: ").strip()
 
-        autor = input("Autor: ").strip()
-        while autor == "":
-            print("El autor no puede estar vacio")
-            autor = input("autor: ").strip()
+#        autor = input("Autor: ").strip()
+#        while autor == "":
+#            print("El autor no puede estar vacio")
+#            autor = input("autor: ").strip()
 
-        anio = input("Año: ").strip()
-        while not anio.isdigit():
-            print("El año no puede estar vacio")
-            anio = input("Año: ").strip()
+#        anio = input("Año: ").strip()
+#        while not anio.isdigit():
+#            print("El año no puede estar vacio")
+#            anio = input("Año: ").strip()
 
-        libros = libros + [[titulo, autor, anio]]
-        print("Libro registrado con exito")
+#        libros = libros + [[titulo, autor, anio]]
+#        print("Libro registrado con exito")
 
     #Mostrar los libros ----------
-    elif opcion == "2":
-        if libros == []:
-            print("No Hay libros en la lista")
-        else:
-            numero = 1
-            for libro in libros:
-                print(numero, "Titulo", libro[0].upper(), "| Autor:", libro[1].title(), "| Año", libro[2])
-                numero += 1
-            print("---------------------")
+#    elif opcion == "2":
+#        if libros == []:
+#            print("No Hay libros en la lista")
+#        else:
+#            numero = 1
+#            for libro in libros:
+#                print(numero, "Titulo", libro[0].upper(), "| Autor:", libro[1].title(), "| Año", libro[2])
+#                numero += 1
+#            print("---------------------")
 
     #Buscar libro ----------------
-    elif opcion == "3":
-        if libros == []:
-            print("No Hay libros en la lista")
-        else:
-            buscado = input("Ingresa el titulo a buscar: ").strip().lower()
-            encontrado = 0
-            for libros in libros:
-                if buscado in libro[0].lower():
-                    encontrado = encontrado + 1
-                    print("Titulo", libro[0].upper(), "| Autor:", libro[1].title(), "| Año", libro[2])
-            if encontrado == 0:
-                print("No se encontraron resultados")
-            else:
-                print("Se encontraron", encontrado, "coincidencia/s")
+#    elif opcion == "3":
+#        if libros == []:
+#            print("No Hay libros en la lista")
+#        else:
+#            buscado = input("Ingresa el titulo a buscar: ").strip().lower()
+#            encontrado = 0
+#            for libros in libros:
+#                if buscado in libro[0].lower():
+#                    encontrado = encontrado + 1
+#                    print("Titulo", libro[0].upper(), "| Autor:", libro[1].title(), "| Año", libro[2])
+#            if encontrado == 0:
+#                print("No se encontraron resultados")
+#            else:
+#                print("Se encontraron", encontrado, "coincidencia/s")
         
 
     #Despedida -------------------
-    elif opcion == "4":
-        print("Gracias por usar el sistema de la biblioteca")
+#    elif opcion == "4":
+#        print("Gracias por usar el sistema de la biblioteca")
 
-    else:
-        print("Opcion invalida. Elegi una opción del 1 al 4")
+#    else:
+#        print("Opcion invalida. Elegi una opción del 1 al 4")
+
+#CLASE 07
+#listas y duplas
+
+#productos = ["pan", "leche", "queso", "yogurt", "manteca"]
+
+#print(productos[-2])
+
+#APPEND
+
+#lista.append(elemento_agregar)
+
+#productos.append("yerba")
+
+#print(productos)
+
+#lista(lista_productos)
+#lista_productos = []
+#
+#continuar = "si"
+
+#while continuar == "si":
+#    nombre = input("Emi")
+#    precio = input("36")
+#    categoria = input("master")
+
+#    lista_productos.append([nombre, precio, categoria])
+#    continuar = input("Queres agregar otro producto? si/no").strip().lower()
+
+
+#print(lista_productos)
+#print(lista_productos[0])
+
+#for producto in lista_productos:
+#    print(producto[0])
+#    print(producto[1])
+#    print(producto[2])
+
+#lista = [["Emi", "36", "Master"], ["Santi", "35", "consulta"]]
+
+#print(lista[1][1])
+
+#lista[1][1] = "36"
+
+#print(lista)
+
+#Remove
+#busca y elimina un valor especifico
+
+#lista.remove(valor_a_eliminar)
+
+#productos = ["pan", "leche", "queso", "yogur", "manteca", "pan", "pan"]
+
+#productos.remove("pan")
+
+#while "pan" in productos:
+#    productos.remove("pan")
+
+#print(productos.count("pan"))
+
+#while productos.count("pan") > 1:
+#    productos.remove("pan")
+
+#print(productos)
+
+#pop(indice)
+
+#productos.pop(1)
+
+#print(productos)
+
+#lista_num = [5,6,9,1,2,3,4]
+
+#lista_num.sort(reverse=True)
+#print(lista_num)
+
+#productos.sort()
+#print(productos)
+
+
+#print(productos.index("manteca"))
+#productos.insert(2, "dulce de leche")
+
+#print(productos)
+
+
+
+#Tuplas (similares a las listas, todo lo que se pueden hacer en las listas con las tuplas no)
+#()
+
+tupla1 = (10,20,30, [])
+
+tupla2 = 40,50,60
+
+#las tuplas pueden guardar todo tipo de datos (numeros enteros, decimales, etc.) pero no se puede modificar dentro de la tupla solo se puede buscar
+
+#nombre = input("nombre")
+#precio = input("precio")
+#categoria = input("categoria")
+
+#tupla_prod = (nombre, precio, categoria)
+#tupla1[3].append("Emi")
+#print(tupla1[2])
+#tupla1[2] = 50
+#print(tupla1[2])
+
+
+lista_tupla = list(tupla1)
+print(lista_tupla)
+lista_tupla = tuple(tupla1)
+print(lista_tupla)
+
+tupla3 = (1,)
